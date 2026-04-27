ded3d51c79034e9530a284872c66f1ea23b24a9f
KamiDeveloper <medrano.9894276@gmail.com>
2026-04-27 01:20:17 -0400

    feat(03-02): reading components index export

---
+++ components/reading/index.ts
@@ -0,0 +1,10 @@
+// components/reading/index.ts
+// Barrel export for all reading components.
+
+export { RubyKanji } from './RubyKanji';
+export { JapaneseTextRenderer } from './JapaneseTextRenderer';
+export { TranslationPanel } from './TranslationPanel';
+export { KanjiDetail } from './KanjiDetail';
+
+export type { TranslationPanelProps } from './TranslationPanel';
+export type { KanjiDetailProps } from './KanjiDetail';
