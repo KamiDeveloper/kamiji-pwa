a55d7f0d680a82996c677a8a6e5227a28439bef7
KamiDeveloper <medrano.9894276@gmail.com>
2026-04-27 01:30:16 -0400

    feat(03-03): update reading components index

--- components/reading/index.ts
+++ components/reading/index.ts
@@ -5,6 +5,12 @@ export { RubyKanji } from './RubyKanji';
 export { JapaneseTextRenderer } from './JapaneseTextRenderer';
 export { TranslationPanel } from './TranslationPanel';
 export { KanjiDetail } from './KanjiDetail';
+export { ReadingHeader } from './ReadingHeader';
+export { KanjiSummaryFooter } from './KanjiSummaryFooter';
+export { PageIndicator } from './PageIndicator';
+export { ReadingView } from './ReadingView';
+export { ReadingViewClient } from './ReadingViewClient';
 
 export type { TranslationPanelProps } from './TranslationPanel';
 export type { KanjiDetailProps } from './KanjiDetail';
+export type { ReadingViewProps, Story } from './ReadingView';
